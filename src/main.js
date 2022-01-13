229749c3d09ba8c25125c82bf2f2f50c0970b590
JerzyTian <704750620@qq.com>
2022-01-13 15:58:08 +0800

    2

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import router from './router/index'
+// 导入全局样式
+import './assets/css/global.css'
+// 导入elementui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
